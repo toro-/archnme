70746b982d650e01009656b263bbd081dde51766
toro- <toro59@hotmail.com>
2022-11-18 23:25:52 +0000

    Update archnmeinstall.sh

--- archnmeinstall.sh
+++ archnmeinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pacstrap -K /mnt base base-devel linux linux-firmware vim sudo networkmanager man-db man-pages gnome
+# pacstrap -K /mnt base base-devel linux linux-firmware vim sudo networkmanager man-db man-pages
 
-echo archnme >> /mnt/etc/hostname
+echo "archnme" >> /mnt/etc/hostname
