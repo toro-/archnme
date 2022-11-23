f15ff94fe532500aeba1538846c66423d95e73f4
toro- <toro59@hotmail.com>
2022-11-23 23:48:27 +0000

    Update archnmeinstall.sh

--- archnmeinstall.sh
+++ archnmeinstall.sh
@@ -3,3 +3,9 @@
 # pacstrap -K /mnt base base-devel linux linux-firmware vim sudo networkmanager man-db man-pages
 
 echo "archnme" >> /mnt/etc/hostname
+
+echo -e "Section \"InputClass\"
+       Identifier \"keyboard0\"
+       Driver	\"kbd\"
+       		Option	\"XkbModel\" \"pc105\"
+		Option	\"XkbLayout\" \"gb\""
