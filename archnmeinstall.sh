7866da5533df411c17115de95075b34f5008f038
toro- <toro59@hotmail.com>
2022-11-23 23:55:13 +0000

    Update archnmeinstall.sh

--- archnmeinstall.sh
+++ archnmeinstall.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# pacstrap -K /mnt base base-devel linux linux-firmware vim sudo networkmanager man-db man-pages
 
+
+pacstrap -K /mnt base base-devel linux linux-firmware vim sudo networkmanager man-db man-pages
+
+## Set hostname
 echo "archnme" >> /mnt/etc/hostname
 
+## Set locale info for Xfce4 + lightdm
 echo -e "Section \"InputClass\"
        Identifier \"keyboard0\"
        Driver	\"kbd\"
        		Option	\"XkbModel\" \"pc105\"
-		Option	\"XkbLayout\" \"gb\""
+		Option	\"XkbLayout\" \"gb\"" > /mnt/etc/X11/xorg.conf.d/20-keyboard.conf
